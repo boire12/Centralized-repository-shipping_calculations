c417a7b83b633bacc3672fbabbf034956b7676b3
boire12 <tristanboire@gmail.com>
2024-02-21 20:53:32 -0500

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,5 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # new update by Boire12
+git config --global user.email "tristanboire@gmail.com"
+git config --global user.name "boire12"
